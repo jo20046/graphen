0e33e83c877abd3bf19729460b762b7af3743e88
Enthöfer Joshua <j.enthoefer@laudert.de>
2019-06-07 12:40:23 +0200

    - Aufgabe 8.2

--- main.cpp
+++ main.cpp
@@ -14,41 +14,23 @@ int main() {
     graph.add_knoten(2);
     graph.add_knoten(3);
     graph.add_knoten(4);
+    graph.add_knoten(5);
+    graph.add_knoten(6);
     graph.add_kante(0, 1);
+    graph.add_kante(0, 5);
     graph.add_kante(1, 2);
     graph.add_kante(1, 4);
+    graph.add_kante(1, 5);
+    graph.add_kante(2, 6);
     graph.add_kante(3, 4);
+    graph.add_kante(4, 6);
 
-//    Adjazenzmatrix adjazenzmatrix{graph};
-//    adjazenzmatrix.print();
-//    adjazenzmatrix.to_graphviz();
-
-//    Inzidenzmatrix inzidenzmatrix{graph};
-//    inzidenzmatrix.print();
-//    inzidenzmatrix.to_graphviz();
-//
-//    Kantentabelle kantentabelle{graph};
-//    kantentabelle.print();
-//    kantentabelle.to_graphviz();
-
-//    Adjazenzmatrix adjazenzmatrix{graph};
-//    Inzidenzmatrix inzidenzmatrix = Converter::adjazenzmatrix_to_inzidenzmatrix(adjazenzmatrix);
-//    inzidenzmatrix.print();
-//    Kantentabelle kantentabelle = Converter::adjazenzmatrix_to_kantentabelle(adjazenzmatrix);
-//    kantentabelle.print();
-
-//    Inzidenzmatrix inzidenzmatrix{graph};
-//    Adjazenzmatrix adjazenzmatrix = Converter::inzidenzmatrix_to_adjazenzmatrix(inzidenzmatrix);
-//    adjazenzmatrix.print();
-//    Kantentabelle kantentabelle = Converter::inzidenzmatrix_to_kantentabelle(inzidenzmatrix);
-//    kantentabelle.print();
-
-    Kantentabelle kantentabelle{graph};
-//    Adjazenzmatrix adjazenzmatrix = Converter::kantentabelle_to_adjazenzmatrix(kantentabelle);
-//    adjazenzmatrix.print();
-    Inzidenzmatrix inzidenzmatrix = Converter::kantentabelle_to_inzidenzmatrix(kantentabelle);
-    inzidenzmatrix.print();
-
+    Adjazenzmatrix adjazenzmatrix{graph};
+    Inzidenzmatrix inzidenzmatrix = Converter::adjazenzmatrix_to_inzidenzmatrix(adjazenzmatrix);
+    Kantentabelle kantentabelle = Converter::adjazenzmatrix_to_kantentabelle(adjazenzmatrix);
+    adjazenzmatrix.to_graphviz();
+    inzidenzmatrix.to_graphviz();
+    kantentabelle.to_graphviz();
 
 
 
